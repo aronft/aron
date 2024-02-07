50da9f70a8058d9e7329a32e88e1f8d8d3a49979
blazte <blazte.aster@gmail.com>
2024-02-07 12:11:03 -0500

    add colors and boxshadow to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,13 +9,30 @@ export default {
 				sans: ['Inter'],
 				hand: ['Handlee']
 			},
-			colors: {
-				neutral: {
-					900: '#0C0C0C'
-				}
-			},
 			backgroundImage: {
 				pattern: "url('./src/assets/bg-patterns.png')"
+			},
+			boxShadow: {
+				sm: '5px 5px 0px 0px rgba(0, 0, 0, 1)'
+			}
+		},
+		colors: {
+			black: colors.black,
+			white: colors.white,
+			gray: {
+				...colors.gray,
+				500: '#706f6f'
+			},
+			neutral: {
+				900: '#0C0C0C'
+			},
+			pink: { 200: '#FFC9F0' },
+			amber: {
+				200: '#FFE68C',
+				300: '#eecd56'
+			},
+			sky: {
+				200: '#9DDCFF'
 			}
 		}
 	},
