621b21e361d7f2acc461ef71eb1d776527326700
blazte <blazte.aster@gmail.com>
2024-02-08 14:24:20 -0500

    change size of text in desktop layout and replace arrow Image for Icon component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,11 @@ export default {
 				sans: ['Inter'],
 				hand: ['Handlee']
 			},
+			fontSize: {
+				xl: ['1.25rem', '1.625'],
+				'2xl': ['1.563rem', '1.625'],
+				'3xl': ['1.953rem', '1.625']
+			},
 			backgroundImage: {
 				pattern: "url('./src/assets/bg-patterns.png')"
 			},
@@ -24,6 +29,15 @@ export default {
 				500: '#706f6f'
 			},
 			neutral: {
+				50: '#F2F2F2',
+				100: '#E3E3E3',
+				200: '#C9C9C9',
+				300: '#ADADAD',
+				400: '#949494',
+				500: '#787878',
+				600: '#5C5C5C',
+				700: '#424242',
+				800: '#262626',
 				900: '#0C0C0C'
 			},
 			pink: { 200: '#FFC9F0' },
